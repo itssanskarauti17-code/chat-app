bce22029f0c2d55566dd0fadc32f5e106f354e6a
Sanskar Auti <its.sanskar.auti.17@gmail.com>
2026-05-13 21:14:40 +0530

    Fixed server.js and package.json

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // ================= DB =================
 const dbURI =
